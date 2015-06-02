f260790318368b0f72ec6ed33a3e545f17173bff
owent <owt5008137@live.com>
2015-06-02 14:21:15 +0800

    增加通用的文件系统接口

---
+++ include/Common/FileSystem.h
@@ -0,0 +1,133 @@
+/**
+ * @file FileSystem.h
+ * @brief 文件系统统一接口
+ * Licensed under the MIT licenses.
+ *
+ * @version 1.0
+ * @author OWenT
+ * @date 2014.12.15
+ *
+ * @history
+ *
+ *
+ */
+
+#ifndef _UTIL_COMMON__FILESYSTEM_H
+#define _UTIL_COMMON__FILESYSTEM_H
+
+#pramge once
+
+#include <string>
+#include <vector>
+#include <climits>
+#include <cstdio>
+
+#ifdef _MSC_VER
+#include <io.h>
+#include <direct.h>
+#else
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
+
+namespace util {
+    class FileSystem {
+    public:
+        static const char DIRECTORY_SEPARATOR =
+            #ifdef WIN32
+                '\\';
+            #else
+                '/';
+            #end
+
+        static const size_t MAX_PATH_LEN =
+#if defined(MAX_PATH)
+            MAX_PATH;
+#elif defined(_MAX_PATH)
+            _MAX_PATH;
+#elif defined(PATH_MAX)
+            PATH_MAX;
+#else
+            260;
+#end
+
+    public:
+        /**
+         * @brief 获取文件内容
+         * @param out [OUT] 输出变量
+         * @param file_path [IN] 文件路径
+         * @param is_binary [IN] 是否是二进制
+         * @return 成功返回true
+         */
+        static bool GetFileContent(std::string& out, const char* file_path, bool is_binary = false);
+
+        /**
+         * @brief 获取文件内容
+         * @param out [OUT] 输出变量
+         * @param path [IN] 路径
+         * @param compact [IN] 是否精简路径（这个功能会尽量移除路径中的.和..）
+         * @return 成功返回true
+         */
+        static bool SplitPath(std::vector<std::string>& out, const char* path, bool compact = false);
+
+        /**
+         * @brief 检查文件是否存在
+         * @param file_path [IN] 文件路径
+         * @return 存在且有权限返回true
+         */
+        static bool IsExist(const char* file_path);
+
+        /**
+         * @brief 创建目录
+         * @param dir_path [IN] 目录路径
+         * @param recursion [IN] 是否递归创建
+         * @param mode [IN] 目录权限（Windows下会被忽略）
+         * @return 创建成功返回true
+         */
+        static bool Mkdir(const char* dir_path, bool recursion = false, int mode = 0);
+
+        /**
+         * @brief 获取当前运行目录
+         * @return 当前运行目录
+         */
+        static std::string GetCWD();
+
+        /**
+         * @brief 获取绝对路径
+         * @param dir_path 相对路径
+         * @return 当前运行目录
+         */
+        static std::string GetAbsPath(const char* dir_path);
+
+        /**
+         * @brief 移动或重命名文件/目录
+         * @param from 原始路径
+         * @param to 目标路径
+         * @return 成功返回true
+         */
+        static bool Rename(const char* from, const char* to);
+
+        /**
+         * @brief 移除文件/目录
+         * @param path 路径
+         * @return 成功返回true
+         */
+        static bool Remove(const char* path);
+
+        /**
+         * @brief 打开一个临时文件
+         * @return 临时文件
+         */
+        static FILE* OpenTmpFile();
+
+        /**
+         * @brief 生成一个临时文件路径
+         * @return 临时文件路径
+         */
+        static std::string GenerateTmpFilePath();
+    };
+}
+
+
+#endif //_UTIL_COMMON__FILESYSTEM_H
---
+++ src/Common/FileSystem.cpp
@@ -0,0 +1,158 @@
+//
+// Created by 欧文韬 on 2015/6/2.
+//
+
+#include <cstdio>
+#include "Common/FileSystem.h"
+
+#ifdef _MSC_VER
+#define FUNC_ACCESS(x) _access(x, 0)
+#define SAFE_STRTOK_S(...) strtok_s(__VA_ARGS__)
+#define FUNC_MKDIR(path, mode) _mkdir(path)
+
+#else
+
+#define FUNC_ACCESS(x) access(x, F_OK)
+#define SAFE_STRTOK_S(...) strtok_r(__VA_ARGS__)
+#define FUNC_MKDIR(path, mode) mkdir(path, mode)
+
+#endif
+
+namespace util {
+    std::string FileSystem::GetFileContent(std::string& out, const char* file_path, bool is_binary) {
+        FILE* f = NULL;
+        if(is_binary) {
+            f = fopen(path, "rb");
+        } else {
+            f = fopen(path, "r");
+        }
+
+        if (NULL == f) {
+            return false;
+        }
+
+        fseek(f, 0, SEEK_END);
+        long len = ftell(f);
+        fseek(f, 0, SEEK_SET);
+
+        out.resize(static_cast<size_t>(len));
+        fread(const_cast<char*>(out.data()), sizeof(char), static_cast<size_t>(len), f);
+
+        fclose(f);
+
+        return true;
+    }
+
+    bool FileSystem::SplitPath(std::vector<std::string>& out, const char* path, bool compact) {
+        if (NULL == path) {
+            return false;
+        }
+
+        char opr_path[MAX_PATH_LEN];
+        strncpy(opr_path, path, sizeof(opr_path));
+
+        char* saveptr = NULL;
+        char* token = SAFE_STRTOK_S(opr_path, "\\/", &saveptr);
+        while (NULL != token) {
+            if (0 != strlen(token)) {
+
+                if (compact) {
+                    // 紧缩路径
+                    if (0 == strcmp("..", token)) {
+                        if (!out.empty() && out.back() != "..") {
+                            out.pop_back();
+                        } else {
+                            out.push_back(token);
+                        }
+                    } else if (0 != strcmp(".", token)) {
+                        out.push_back(token);
+                    }
+                } else {
+                    out.push_back(token);
+                }
+            }
+            token = SAFE_STRTOK_S(NULL, "\\/", &saveptr);
+        }
+
+        return !out.empty();
+    }
+
+    bool FileSystem::IsExist(const char* file_path) {
+        return 0 == FUNC_ACCESS(file_path);
+    }
+
+    bool Mkdir(const char* dir_path, bool recursion, int mode) {
+#ifndef _MSC_VER
+        if (0 == mode) {
+            mode = S_IRWXU | S_IRWXG | S_IRGRP | S_IWGRP | S_IROTH;
+        }
+#endif
+        if (!recursion) {
+            return 0 == FUNC_MKDIR(dir_path, mode);
+        }
+
+        std::vector<std::string> path_segs;
+        SplitPath(path_segs, dir_path, true);
+
+        if (path_segs.empty()) {
+            return false;
+        }
+
+        std::string now_path;
+        // 留一个\0和一个分隔符位
+        now_path.reserve(strlen(dir_path) + 2);
+
+        for(size_t i = 0; i < path_segs.size(); ++ i) {
+            now_path += path_segs[i];
+
+            if (false == IsExist(now_path.c_str())) {
+                if (false == Mkdir(now_path.c_str(), false, mode)) {
+                    return false;
+                }
+            }
+
+            now_path += DIRECTORY_SEPARATOR;
+        }
+
+        return true;
+    }
+
+    std::string FileSystem::GetCWD() {
+        std::string ret;
+#ifdef _MSC_VER
+        ret = _getcwd( NULL, 0 );
+#else
+        ret = getcwd( NULL, 0 );
+#endif
+
+        return ret;
+    }
+
+    std::string FileSystem::GetAbsPath(const char* dir_path) {
+        std::string ret;
+        ret.reserve(MAX_PATH_LEN + 1, 0);
+
+#ifdef _MSC_VER
+         _fullpath(const_cast<char*>(ret.data()), dir_path, ret.size());
+#else
+        readlink(dir_path, const_cast<char*>(ret.data()), ret.size());
+#endif
+        return ret;
+    }
+
+    bool FileSystem::Rename(const char* from, const char* to) {
+        return 0 == rename(from, to);
+    }
+
+    bool FileSystem::Remove(const char* path) {
+        return 0 == remove(path);
+    }
+
+    FILE* FileSystem::OpenTmpFile() {
+        return tmpfile();
+    }
+
+    std::string FileSystem::GenerateTmpFilePath() {
+        return tmpnam(NULL);
+    }
+}
